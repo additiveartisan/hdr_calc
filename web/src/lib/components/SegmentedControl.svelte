bca8d128b6b8ace9dd10cd1c1268b96dc77ccd49
additiveartisan <additiveartisan@users.noreply.github.com>
2026-02-14 22:18:48 -0600

    ...

--- web/src/lib/components/SegmentedControl.svelte
+++ web/src/lib/components/SegmentedControl.svelte
@@ -91,10 +91,10 @@
 		-webkit-backdrop-filter: blur(20px);
 		backdrop-filter: blur(20px);
 		transition:
-			left 250ms cubic-bezier(0.2, 1, 0.3, 1),
-			width 250ms cubic-bezier(0.2, 1, 0.3, 1),
+			left 250ms cubic-bezier(0.25, 0.8, 0.35, 1.1),
+			width 250ms cubic-bezier(0.25, 0.8, 0.35, 1.1),
 			background 0ms,
-			transform 250ms cubic-bezier(0.2, 1, 0.3, 1);
+			transform 250ms cubic-bezier(0.25, 0.8, 0.35, 1.1);
 		pointer-events: none;
 	}
 
