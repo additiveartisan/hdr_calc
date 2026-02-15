078b3245bd63f9a298109d0c6cd81575bb15f6ef
additiveartisan <rob@additiveartisan.com>
2026-02-15 16:18:50 -0600

    ...

--- ios/HDRCalc/HDRCalc/HDRCalcApp.swift
+++ ios/HDRCalc/HDRCalc/HDRCalcApp.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 @main
 struct HDRCalcApp: App {
+    @State private var connectionService = CameraConnectionService()
+    @State private var shootingVM = ShootingViewModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(connectionService)
+                .environment(shootingVM)
         }
     }
 }
