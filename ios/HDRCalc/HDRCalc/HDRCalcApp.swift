727bffd475669a1354513a120a26b47c543da763
additiveartisan <additiveartisan@users.noreply.github.com>
2026-02-24 14:28:48 -0600

    ...

--- ios/HDRCalc/HDRCalc/HDRCalcApp.swift
+++ ios/HDRCalc/HDRCalc/HDRCalcApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct HDRCalcApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MockupRunner() // TODO: revert to ContentView()
         }
     }
 }
